d70df3449372cb2af3c1f58b9526b00a30d4024b
admin <rafalohaki@gmail.com>
2023-03-20 05:11:52 +0100

    deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ subprojects {
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
         implementation("com.github.Blatzar:NiceHttp:0.4.2") // http library
         implementation("org.jsoup:jsoup:1.1.15.4") // html parser
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // html parser
         implementation("org.mozilla:rhino:1.7.14")
         implementation ("me.xdrop:fuzzywuzzy:1.4.0")
